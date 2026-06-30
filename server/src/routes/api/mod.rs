c4f4892099cf81d2f03688b58ee92c9c5b56aaab
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-06-30 21:30:26 +0100

    Fix typo

--- server/src/routes/api/mod.rs
+++ server/src/routes/api/mod.rs
@@ -2,12 +2,12 @@ use axum::routing::any;
 use crate::routes::Router;
 
 mod history;
-mod latest_id;
+mod latest_idx;
 mod get_new;
 
 pub fn routes() -> Router {
     Router::new()
         .nest("/history", history::routes())
-        .nest("/latest_idx", latest_id::routes())
+        .nest("/latest_idx", latest_idx::routes())
         .nest("/get_new", get_new::routes())
 }
